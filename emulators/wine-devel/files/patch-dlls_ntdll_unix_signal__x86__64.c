41945fa728303bad1c7a134a6159c0c84091d1e5
Alex S <shkhln@users.noreply.github.com>
2024-01-31 20:30:07 +0300

    That might be a bit better.

--- emulators/wine-devel/files/patch-dlls_ntdll_unix_signal__x86__64.c
+++ emulators/wine-devel/files/patch-dlls_ntdll_unix_signal__x86__64.c
@@ -1,23 +1,15 @@
 --- dlls/ntdll/unix/signal_x86_64.c.orig	2024-01-06 00:20:29.000000000 +0300
-+++ dlls/ntdll/unix/signal_x86_64.c	2024-01-28 19:36:26.807058000 +0300
-@@ -18,6 +18,8 @@
-  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
-  */
- 
-+#define USE_FS
-+
- #if 0
- #pragma makedep unix
- #endif
-@@ -144,6 +146,7 @@ __ASM_GLOBAL_FUNC( alloc_fs_sel,
++++ dlls/ntdll/unix/signal_x86_64.c	2024-01-31 20:28:38.135291000 +0300
+@@ -144,6 +144,8 @@ __ASM_GLOBAL_FUNC( alloc_fs_sel,
  
  #elif defined(__FreeBSD__) || defined (__FreeBSD_kernel__)
  
++#include <machine/cpufunc.h>
 +#include <machine/segments.h>
  #include <machine/trap.h>
  
  #define RAX_sig(context)     ((context)->uc_mcontext.mc_rax)
-@@ -454,7 +457,7 @@ static inline struct amd64_thread_data *amd64_thread_d
+@@ -454,7 +456,7 @@ static inline struct amd64_thread_data *amd64_thread_d
      return (struct amd64_thread_data *)ntdll_get_thread_data()->cpu_data;
  }
  
@@ -26,34 +18,40 @@
  static inline TEB *get_current_teb(void)
  {
      unsigned long rsp;
-@@ -825,6 +828,17 @@ static inline ucontext_t *init_handler( void *sigconte
+@@ -824,6 +826,18 @@ static inline ucontext_t *init_handler( void *sigconte
+         struct ntdll_thread_data *thread_data = (struct ntdll_thread_data *)&get_current_teb()->GdiTebBatch;
          arch_prctl( ARCH_SET_FS, ((struct amd64_thread_data *)thread_data->cpu_data)->pthread_teb );
      }
++#elif defined(__FreeBSD__)
++  struct ntdll_thread_data *thread_data = (struct ntdll_thread_data *)&get_current_teb()->GdiTebBatch;
++  USHORT fs = ((struct amd64_thread_data *)thread_data->cpu_data)->fs;
++  if (fs != 0 && fs != GSEL(GUFS32_SEL, SEL_UPL))
++  {
++      load_fs(GSEL(GUFS32_SEL, SEL_UPL));
++      amd64_set_fsbase(((struct amd64_thread_data *)thread_data->cpu_data)->pthread_teb);
++  }
++  else
++  {
++      assert(rdfsbase() == ((struct amd64_thread_data *)thread_data->cpu_data)->pthread_teb); // ?
++  }
  #endif
-+#ifdef __FreeBSD__
-+    if (fs32_sel)
-+    {
-+        struct ntdll_thread_data *thread_data = (struct ntdll_thread_data *)&get_current_teb()->GdiTebBatch;
-+#ifdef USE_FS
-+        USHORT sel = GSEL(GUFS32_SEL, SEL_UPL);
-+        __asm__ volatile("movw %0,%%fs" :: "r" (sel));
-+#endif
-+        amd64_set_fsbase(((struct amd64_thread_data *)thread_data->cpu_data)->pthread_teb);
-+    }
-+#endif
      return sigcontext;
  }
- 
-@@ -834,7 +848,7 @@ static inline ucontext_t *init_handler( void *sigconte
-  */
- static inline void leave_handler( ucontext_t *sigcontext )
- {
--#ifdef __linux__
-+#if defined(__linux__) || defined(__FreeBSD__)
+@@ -837,6 +851,13 @@ static inline void leave_handler( ucontext_t *sigconte
+ #ifdef __linux__
      if (fs32_sel && !is_inside_signal_stack( (void *)RSP_sig(sigcontext )) && !is_inside_syscall(sigcontext))
          __asm__ volatile( "movw %0,%%fs" :: "r" (fs32_sel) );
++#elif defined(__FreeBSD__)
++    struct ntdll_thread_data *thread_data = (struct ntdll_thread_data *)&get_current_teb()->GdiTebBatch;
++    USHORT fs = ((struct amd64_thread_data *)thread_data->cpu_data)->fs;
++    if (fs != 0 && !is_inside_signal_stack((void *)RSP_sig(sigcontext)) && !is_inside_syscall(sigcontext))
++    {
++        load_fs(fs);
++    }
  #endif
-@@ -1598,7 +1612,7 @@ __ASM_GLOBAL_FUNC( call_user_mode_callback,
+ #ifdef DS_sig
+     DS_sig(sigcontext) = ds64_sel;
+@@ -1598,7 +1619,7 @@ __ASM_GLOBAL_FUNC( call_user_mode_callback,
                     "movq %rsp,0x328(%r8)\n\t"  /* amd64_thread_data()->syscall_frame */
                     /* switch to user stack */
                     "movq %rdi,%rsp\n\t"        /* user_rsp */
@@ -62,7 +60,7 @@
                     "testl $12,%r14d\n\t"       /* SYSCALL_HAVE_PTHREAD_TEB | SYSCALL_HAVE_WRFSGSBASE */
                     "jz 1f\n\t"
                     "movw 0x338(%r8),%fs\n"     /* amd64_thread_data()->fs */
-@@ -2174,6 +2188,7 @@ static void usr1_handler( int signal, siginfo_t *sigin
+@@ -2174,6 +2195,7 @@ static void usr1_handler( int signal, siginfo_t *sigin
   *           LDT support
   */
  
@@ -70,12 +68,12 @@
  #define LDT_SIZE 8192
  
  #define LDT_FLAGS_DATA      0x13  /* Data segment */
-@@ -2231,6 +2246,16 @@ static void ldt_set_entry( WORD sel, LDT_ENTRY entry )
+@@ -2231,6 +2253,16 @@ static void ldt_set_entry( WORD sel, LDT_ENTRY entry )
  
  #if defined(__APPLE__)
      if (i386_set_ldt(index, (union ldt_entry *)&entry, 1) < 0) perror("i386_set_ldt");
 +#elif defined(__FreeBSD__)
-+  	struct i386_ldt_args p;
++    struct i386_ldt_args p;
 +    p.start = index;
 +    p.descs = (struct user_segment_descriptor *)&entry;
 +    p.num   = 1;
@@ -87,15 +85,7 @@
  #else
      fprintf( stderr, "No LDT support on this platform\n" );
      exit(1);
-@@ -2299,6 +2324,7 @@ NTSTATUS signal_alloc_thread( TEB *teb )
-             server_leave_uninterrupted_section( &ldt_mutex, &sigset );
-             if (idx == LDT_SIZE) return STATUS_TOO_MANY_THREADS;
-             thread_data->fs = (idx << 3) | 7;
-+            //~ fs32_sel = thread_data->fs; // ?
-         }
-         else thread_data->fs = fs32_sel;
-     }
-@@ -2378,7 +2404,6 @@ static void *mac_thread_gsbase(void)
+@@ -2378,7 +2410,6 @@ static void *mac_thread_gsbase(void)
  }
  #endif
  
@@ -103,27 +93,26 @@
  /**********************************************************************
   *		signal_init_process
   */
-@@ -2414,7 +2439,7 @@ void signal_init_process(void)
-         }
-         else ERR_(seh)( "failed to allocate %%fs selector\n" );
-     }
--#elif defined(__APPLE__)
-+#elif defined(__APPLE__) || defined(__FreeBSD__)
-     if (wow_teb)
-     {
-         LDT_ENTRY cs32_entry, fs32_entry;
-@@ -2438,6 +2463,10 @@ void signal_init_process(void)
-             ldt_set_entry( amd64_thread_data()->fs, fs32_entry );
+@@ -2439,6 +2470,18 @@ void signal_init_process(void)
              break;
          }
-+#ifdef __FreeBSD__
-+        //~ fs32_sel = amd64_thread_data()->fs;
-+        syscall_flags |= SYSCALL_HAVE_PTHREAD_TEB;
-+#endif
      }
++#elif defined(__FreeBSD__)
++    if (wow_teb)
++    {
++        LDT_ENTRY fs32_entry = ldt_make_entry(wow_teb, page_size - 1, LDT_FLAGS_DATA | LDT_FLAGS_32BIT);
++
++        cs32_sel = GSEL(GUCODE32_SEL, SEL_UPL);
++
++        amd64_thread_data()->fs = (first_ldt_entry << 3) | 7;
++        ldt_set_entry(amd64_thread_data()->fs, fs32_entry);
++
++        syscall_flags |= SYSCALL_HAVE_PTHREAD_TEB;
++    }
  #endif
  
-@@ -2484,8 +2513,9 @@ void call_init_thunk( LPTHREAD_START_ROUTINE entry, vo
+     sig_act.sa_mask = server_block_set;
+@@ -2484,8 +2527,9 @@ void call_init_thunk( LPTHREAD_START_ROUTINE entry, vo
      arch_prctl( ARCH_SET_GS, teb );
      arch_prctl( ARCH_GET_FS, &thread_data->pthread_teb );
      if (fs32_sel) alloc_fs_sel( fs32_sel >> 3, get_wow_teb( teb ));
@@ -135,7 +124,7 @@
  #elif defined(__NetBSD__)
      sysarch( X86_64_SET_GSBASE, &teb );
  #elif defined (__APPLE__)
-@@ -2588,7 +2618,6 @@ __ASM_GLOBAL_FUNC( signal_start_thread,
+@@ -2588,7 +2632,6 @@ __ASM_GLOBAL_FUNC( signal_start_thread,
                     "1:\tmovq %r8,%rsp\n\t"
                     "call " __ASM_NAME("call_init_thunk"))
  
@@ -143,7 +132,7 @@
  /***********************************************************************
   *           __wine_syscall_dispatcher
   */
-@@ -2632,7 +2661,11 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
+@@ -2632,7 +2675,11 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
                     __ASM_CFI_REG_IS_AT2(rbp, rcx, 0x98, 0x01)
                     /* Legends of Runeterra hooks the first system call return instruction, and
                      * depends on us returning to it. Adjust the return address accordingly. */
@@ -155,18 +144,15 @@
                     "movl 0xb0(%rcx),%r14d\n\t"     /* frame->syscall_flags */
                     "testl $3,%r14d\n\t"            /* SYSCALL_HAVE_XSAVE | SYSCALL_HAVE_XSAVEC */
                     "jz 2f\n\t"
-@@ -2696,6 +2729,26 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
+@@ -2696,6 +2743,23 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
                     "leaq -0x98(%rbp),%rcx\n"
                     "2:\n\t"
  #endif
 +#ifdef __FreeBSD__
 +                   "testl $12,%r14d\n\t"           /* SYSCALL_HAVE_PTHREAD_TEB | SYSCALL_HAVE_WRFSGSBASE */
 +                   "jz 2f\n\t"
-+
-+# ifdef USE_FS
 +                   "movq $0x13,%rsi\n\t"           /* GSEL(GUFS32_SEL, SEL_UPL) */
 +                   "movq %rsi,%fs\n\t"
-+# endif
 +# if USE_AMD64_SET_FSBASE_FUNC
 +                   "pushq %rcx\n\t"
 +                   "movq %gs:0x320,%rdi\n\t"       /* amd64_thread_data()->pthread_teb */
@@ -182,7 +168,7 @@
                     "movq 0x00(%rcx),%rax\n\t"
                     "movq 0x18(%rcx),%r11\n\t"      /* 2nd argument */
                     "movl %eax,%ebx\n\t"
-@@ -2768,7 +2821,7 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
+@@ -2768,7 +2832,7 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
                     "movq 0x20(%rcx),%rsi\n\t"
                     "movq 0x08(%rcx),%rbx\n\t"
                     "leaq 0x70(%rcx),%rsp\n\t"      /* %rsp > frame means no longer inside syscall */
@@ -191,17 +177,15 @@
                     "testl $12,%r14d\n\t"           /* SYSCALL_HAVE_PTHREAD_TEB | SYSCALL_HAVE_WRFSGSBASE */
                     "jz 1f\n\t"
                     "movw %gs:0x338,%fs\n"          /* amd64_thread_data()->fs */
-@@ -2902,6 +2955,22 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
+@@ -2902,6 +2966,20 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
                     "syscall\n\t"
                     "2:\n\t"
  #endif
 +#ifdef __FreeBSD__
 +                   "testl $12,%r14d\n\t"           /* SYSCALL_HAVE_PTHREAD_TEB | SYSCALL_HAVE_WRFSGSBASE */
 +                   "jz 2f\n\t"
-+# ifdef USE_FS
 +                   "movq $0x13,%rsi\n\t"           /* GSEL(GUFS32_SEL, SEL_UPL) */
 +                   "movq %rsi,%fs\n\t"
-+# endif
 +# if USE_AMD64_SET_FSBASE_FUNC
 +                   "movq %gs:0x320,%rdi\n\t"       /* amd64_thread_data()->pthread_teb */
 +                   "callq amd64_set_fsbase\n\t"
@@ -214,7 +198,7 @@
                     "movq %r8,%rdi\n\t"             /* args */
                     "callq *(%r10,%rdx,8)\n\t"
                     "movq %rsp,%rcx\n\t"
-@@ -2920,7 +2989,7 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
+@@ -2920,7 +2998,7 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
                     /* switch to user stack */
                     "movq 0x88(%rcx),%rsp\n\t"
                     __ASM_CFI(".cfi_restore_state\n\t")
